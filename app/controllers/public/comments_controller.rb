6010bc000e0e9155c72b09342f18e4c3d9a4e134
chimu-a <138343606+chimu-a@users.noreply.github.com>
2023-09-19 12:43:54 +0000

    [add]いいね・コメントの非同期化

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -3,17 +3,18 @@ class Public::CommentsController < ApplicationController
   end
 
   def create
-    post_item = PostItem.find(params[:post_item_id])
+    @post_item = PostItem.find(params[:post_item_id])
     comment = current_customer.comments.new(comment_params)
-    comment.post_item_id = post_item.id
+    comment.post_item_id = @post_item.id
     comment.save
-    post = post_item.post
-    redirect_to post_path(post)
+    @post = @post_item.post
+    # redirect_to post_path(post)
   end
 
   def destroy
+    @post_item = PostItem.find(params[:post_item_id])
     Comment.find(params[:id]).destroy
-    redirect_to post_path(params[:post_item_id])
+    # redirect_to post_path(params[:post_item_id])
   end
 
   def comment_params
